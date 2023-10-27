b821567557da7ee32a72b582ec37a236afc845fa
webntu <manocode88@gmail.com>
2023-10-28 00:36:08 +0200

    Clean code.

--- djangoFirebaseModels/settings.py
+++ djangoFirebaseModels/settings.py
@@ -82,8 +82,7 @@ WSGI_APPLICATION = 'djangoFirebaseModels.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 
